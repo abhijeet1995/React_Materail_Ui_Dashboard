636c89a88f1b92a8deb156f10b9fb86137197ece
Abhijeet Shikhar <shikharabhijeet1995@gmail.com>
2020-07-23 21:17:36 +0530

    --

--- src/Auth/Login/Login.jsx
+++ src/Auth/Login/Login.jsx
@@ -24,7 +24,7 @@ const Login = ({isAuthenticated, login, alert, loading, load }) => {
 	})
 
 	const {  email, password } = formData;
-
+//a
 	const handleChange = name => event => {
 		setFormData({ ...formData, [name]: event.target.value });
 	};
